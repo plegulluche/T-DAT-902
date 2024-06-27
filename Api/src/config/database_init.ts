f7c69eb6ba4d1a72001834449b3f0e5485b3a4e5
plegulluche <plegulluche@gmail.com>
2024-06-27 13:37:57 +0200

    [ADD] adding method to initialize the database in mongo

--- Api/src/config/database_init.ts
+++ Api/src/config/database_init.ts
@@ -1,1 +1,22 @@
-export async function initializeDatabase() {}
+import { MongoClient } from 'mongodb'
+
+export async function initializeDatabase() {
+  const url = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@localhost:${process.env.MONGO_PORT}`
+  const client = new MongoClient(url)
+
+  try {
+    await client.connect()
+    const dbName = 'mongo_t_dat_902'
+    const db = client.db(dbName)
+
+    // Création d'une collection de test pour s'assurer que la base de données est initialisée
+    await db.createCollection('test_collection')
+
+    console.log(`Database ${dbName} initialized successfully.`)
+  } catch (error) {
+    console.error('Error initializing database:', error)
+    throw error
+  } finally {
+    await client.close()
+  }
+}
