5136d2e7bfdc2785f25912341a4edfa1ebc671ac
Canjas <mathieu.blais@epitech.eu>
2024-07-17 20:22:39 +0200

    fix

--- Web/src/components/profile.tsx
+++ Web/src/components/profile.tsx
@@ -20,9 +20,9 @@ const Profile: React.FC = () => {
     return (
       <div className="min-h-screen p-5 flex flex-col items-center">
         <div className="bg-white rounded-lg border-2 border-gray-200 p-5 w-full max-w-lg mb-4">
-          <h2 className="text-xl font-semibold mb-8 text-black">My Profile</h2>
+          <h2 className="text-xl font-semibold mb-4 text-black">Profile</h2>
           <form onSubmit={handleProfileUpdate} className="space-y-6">
-            <div className="mb-4">
+            <div className="">
               <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
               <input
                 type="text"
@@ -30,7 +30,7 @@ const Profile: React.FC = () => {
                 placeholder="Enter your name"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
-                className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                className="mt-1 block bg-white w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
                 required
               />
             </div>
@@ -42,7 +42,7 @@ const Profile: React.FC = () => {
                 placeholder="Enter your username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
-                className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                className="mt-1 bg-white block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
                 required
               />
             </div>
@@ -54,11 +54,11 @@ const Profile: React.FC = () => {
                 placeholder="Enter image URL"
                 value={profilePicture}
                 onChange={(e) => setProfilePicture(e.target.value)}
-                className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                className="mt-1 block bg-white w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
                 required
               />
             </div>
-            <button type="submit" className="w-full bg-red-600 text-white py-2 rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">Update Profile</button>
+            <button type="submit" className="w-full bg-black text-white py-2 rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">Update Profile</button>
           </form>
         </div>
         <div className="bg-white rounded-lg rounded-lg border-2 border-gray-200 p-5 w-full max-w-lg">
