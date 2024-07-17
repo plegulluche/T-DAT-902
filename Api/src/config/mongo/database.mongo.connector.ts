fcef87c1103a17ae5fb37403c00eacf2826c4fd8
plegulluche <plegulluche@gmail.com>
2024-07-17 17:45:19 +0200

    [ADD] adding a container to see the mongo database with a brwoser based ui , [ADD] adding pre processing on squaremeter average prices , calculating averages and caching result on mongo document, [ADD] adding a function that will calculate averages and cache them on server start

--- Api/src/config/mongo/database.mongo.connector.ts
+++ Api/src/config/mongo/database.mongo.connector.ts
@@ -1,23 +1,28 @@
-import { MongoClient } from 'mongodb'
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
 if (process.env.NODE_ENV !== 'test') {
   dotenv.config()
 }
 
-const username = process.env.MONGO_INITDB_ROOT_USERNAME || 'defaultuser'
-const password = process.env.MONGO_INITDB_ROOT_PASSWORD || 'defaultpass'
+const username = process.env.MONGO_INITDB_ROOT_USERNAME || 'root'
+const password = process.env.MONGO_INITDB_ROOT_PASSWORD || 'epiket2024'
 const port = process.env.MONGO_PORT || '27017'
+const dbName = 'mongo_t_dat_902'
 
-const url = `mongodb://${username}:${password}@localhost:${port}`
+const url = `mongodb://${username}:${password}@localhost:${port}/${dbName}?authSource=admin`
 
 export async function connectToMongoDatabase() {
   try {
-    const client = await MongoClient.connect(url)
-    console.log('Connected successfully to MongoDB')
-    return client.db()
+    await mongoose.connect(url)
+    console.log('Connected successfully to MongoDB using Mongoose')
   } catch (error) {
     console.error('Could not connect to MongoDB', error)
     throw error
   }
+}
+
+export async function closeMongoConnection() {
+  await mongoose.connection.close()
+  console.log('MongoDB connection closed')
 }
