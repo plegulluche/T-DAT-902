24eb0d2d136e1cbcaf5bdf095a695b7772661e6c
Canjas <mathieu.blais@epitech.eu>
2024-07-18 15:15:25 +0200

    fix

--- Web/src/components/map.tsx
+++ Web/src/components/map.tsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import Header from "./header";
 import M2Price from "./m2Price";
 import Spinner from "./spinner";
+import { DoubleCheck, InfoCircle, ThumbsUp, WarningTriangle } from "iconoir-react";
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWF0aGlldWJsYWlzIiwiYSI6ImNrZjZneDRmdDB3bG4yeHA5ZHN5NDNsYm0ifQ.0-ZZSb86hkNjwGqMJEiF2Q';
 
@@ -34,16 +35,13 @@ const Map: React.FC = () => {
   const mapContainerRef = useRef<HTMLDivElement>(null);
   const [selectedDepartmentId, setSelectedDepartmentId] = useState<string | null>(localStorage.getItem('departement'));
   const [selectedCity, setSelectedCity] = useState<string | null>(null);
+  const [hoveredCity, setHoveredCity] = useState<string | null>(null);
+  const [hovered, setHovered] = useState<string | null>(null);
   const [label, setLabel] = useState<string>();
   const [data, setData] = useState<any>([]);
   const [loading, setLoading] = useState(false);
   const [asChange, setAsChange] = useState(false);
-
-  useEffect(() => {
-    axios.get(`http://localhost:3000/api/squaremeteraverages/year/2023`).then((response) => {
-      console.log(response.data)
-    })
-  }, []);
+  const [densityExpression, setDensityExpression] = useState<any>()
 
   useEffect(() => {
     if (selectedDepartmentId) {
@@ -77,7 +75,6 @@ const Map: React.FC = () => {
         const value2 = piecesTab[pieces];
         setLoading(true)
         axios.get(`http://localhost:3000/api/dvf/price-evolution?inseeCode=${selectedCity}&pieces=${value2}&budget=${value}`).then((response) => {
-          console.log(response.data);
           setData(response.data)
           setLoading(false)
         }
@@ -87,6 +84,19 @@ const Map: React.FC = () => {
     }
     }
   }, [selectedCity, asChange]);
+
+  useEffect(() => {
+    const fetchDensityExpression = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3000/api/squaremeteraverages/year/2023`);
+        setDensityExpression(response.data);
+      } catch (error) {
+        console.error('Error fetching density expression:', error);
+      }
+    };
+
+    fetchDensityExpression();
+  }, []);
 
   useEffect(() => {
     if (mapContainerRef.current) {
@@ -164,7 +174,7 @@ const Map: React.FC = () => {
           layout: {},
           paint: {
             'fill-color': '#FF5733',
-            'fill-opacity': 0
+            'fill-opacity': 0.4
           },
           filter: ['==', 'code', ''],
           maxzoom: 8
@@ -199,6 +209,7 @@ const Map: React.FC = () => {
         map.on('mousemove', 'departements-layer', (e) => {
           if (e.features && e.features.length > 0) {
             const hoveredFeature = e.features[0];
+            setHovered(hoveredFeature.properties?.code as string);
             map.setFilter('departements-hover', ['==', 'code', hoveredFeature.properties?.code as string]);
           }
         });
@@ -206,6 +217,7 @@ const Map: React.FC = () => {
         map.on('mousemove', 'ville-layer', (e) => {
           if (e.features && e.features.length > 0) {
             const hoveredFeature = e.features[0];
+            setHoveredCity(hoveredFeature.properties?.code as string);
             map.setFilter('city-hover', ['==', 'code', hoveredFeature.properties?.code as string]);
           }
         });
@@ -224,10 +236,12 @@ const Map: React.FC = () => {
         });
   
         map.on('mouseleave', 'departements-layer', () => {
+          setHovered(null);
           map.setFilter('departements-hover', ['==', 'code', '']);
         });
 
         map.on('mouseleave', 'ville-layer', () => {
+          setHoveredCity(null);
           map.setFilter('city-hover', ['==', 'code', '']);
         });
 
@@ -235,7 +249,6 @@ const Map: React.FC = () => {
           if (e.features && e.features.length > 0) {
             const clickedFeature = e.features[0];
             setSelectedDepartmentId(clickedFeature.properties?.code as string);
-            console.log(clickedFeature.properties);
             setLabel(clickedFeature.properties?.nom as string);
             map.setFilter('departements-selected', ['==', 'code', clickedFeature.properties?.code as string]);
           }
@@ -257,12 +270,42 @@ const Map: React.FC = () => {
     }
   }, []);
 
+  const getBorderColor = (code: number) => {
+    if (!code) return
+    if (+code < 2000) return 'border-green-500'
+    else if (+code < 3000) return 'border-yellow-500'
+    else if (+code < 4000) return 'border-orange-500'
+    else return 'border-red-500'
+  }
+
+  const getIcon = (code: number) => {
+    if (!code) return
+    if (+code < 2000) return <div>
+      <ThumbsUp className="text-green-500" strokeWidth={2}/>
+    </div>
+    else if (+code < 3000) return <div>
+    <DoubleCheck className="text-yellow-400" strokeWidth={2}/>
+  </div>
+    else if (+code < 4000) return <div>
+    <InfoCircle className="text-orange-400" strokeWidth={2}/>
+  </div>
+    else return <div>
+    <WarningTriangle className="text-red-500" strokeWidth={2}/>
+  </div>
+  }
+
+  console.log(densityExpression)
   return (
     <div>
       <div className="bg-white rounded-lg p-5 flex flex-col border-2 border-gray-200" style={{height: '95dvh'}}>
         <Header label={label} onChange={() => setAsChange(!asChange)}/>
         <div className="h-full flex gap-6 overflow-hidden">
-          <div ref={mapContainerRef} className="rounded-md border-2 border-gray-200 overflow-hidden w-2/3" style={{  height: '100%' }}/>
+          <div ref={mapContainerRef} className="rounded-md border-2 border-gray-200 overflow-hidden w-2/3 relative" style={{  height: '100%' }}>
+            {!hoveredCity && <div className={`bg-white rounded-lg absolute bottom-3 left-3 p-4 shadow border-4 ${getBorderColor(densityExpression?.find((el:any) => el.code_departement === hovered)?.prix_moyen_m2)}`}>
+              {getIcon(densityExpression?.find((el:any) => el.code_departement === hovered)?.prix_moyen_m2)}
+              <p className="text-black text-xl">{densityExpression?.find((el:any) => el.code_departement === hovered)?.prix_moyen_m2} € <span className="text-sm">/m2</span></p>
+            </div>}
+          </div>
           <div className="w-1/3 rounded-lg flex flex-col gap-5">
             <div className="min-h-60 border-2 border-gray-200 rounded-md overflow-hidden w-full p-3">
               <p className="text-black/80 font-semibold text-sm">Price evolution on {label}</p>
